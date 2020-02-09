20c38c2eff652fa6ecdd984f78b84a5ce7824590
1847016090 <1847016090@qq.com>
2020-02-09 17:52:26 +0800

    feat: install and config vant

--- front/src/main.js
+++ front/src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import { Button } from 'vant';
 import App from './App'
 import router from './router'
 
-
-Vue.use(Vant);
+Vue.use(Button)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
